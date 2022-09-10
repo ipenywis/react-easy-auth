d0f256192e01453061560a7fc834e62902eb4f13
Islem Maboud <islempenywis@gmail.com>
2022-09-10 19:19:35 +0100

    Auth

--- src/index.tsx
+++ src/index.tsx
@@ -44,7 +44,7 @@ root.render(
           authType={"cookie"}
           authName={"_auth"}
           cookieDomain={window.location.hostname}
-          cookieSecure={window.location.protocol === "https:"}
+          cookieSecure
         >
           <Centered>
             <BrowserRouter>
